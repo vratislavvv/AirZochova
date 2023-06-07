da9d6a3a2ecf9d366a6f4c74346481a06d98d06e
Ajde <108722871+vratislavvv@users.noreply.github.com>
2023-06-07 16:02:55 +0200

    Add files via upload

---
+++ Air Zochova/backend/letenka.py
@@ -0,0 +1,10 @@
+class letenka:
+
+    def __init__(self, name, surname):
+       self.name = name
+       self.surname = surname
+       print(name)
+       print(surname)
+       pass
+
+y = letenka("Vratislav", "Vojtek")
---
+++ Air Zochova/backend/main.py
@@ -0,0 +1,19 @@
+print("---------------------------------------------------------------")
+print("----------------------- Air Zochova ---------------------------")
+print("---------------------------------------------------------------")
+
+print("Welcome to the booking system of Air Zochova!")
+print("Our airline is currently offering 5 direct flight connections from Bratislava (BTS) to:")
+print("""1. Paris- CDG
+2. London- LHR
+3. Dubai- DBX
+4. Miami- MIA
+5. New York - JFK""")
+
+
+    
+#print("""Paris - CDG 
+#London - HTR, Amsterdam, Miami, New York - JFK""")
+
+
+
