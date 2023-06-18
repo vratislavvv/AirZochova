9a16f34f16ebf7a76d2457be2ac6d88eecc2a703
Ajde <108722871+vratislavvv@users.noreply.github.com>
2023-06-18 17:46:37 +0200

    Update udaje.py

--- Air Zochova/backend/udaje.py
+++ Air Zochova/backend/udaje.py
@@ -3,4 +3,15 @@ destinations = ["Dubai", "DXB", "London", "LHR", "Paris", "CDG", "Miami", "MIA",
 shortrange = ["London", "LHR", "Paris", "CDG"]
 longrange = ["Dubai", "DXB", "Miami", "MIA", "New York", "JFK"]
 
+date1 = None
+date2 = None
+
+sr = False
+lr = False
+
 ticketprice = 0
+
+meno = None
+priezvisko = None
+email = None
+num = None
