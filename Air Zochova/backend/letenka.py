5a2ef6f6f7231cb24b1d16f50b4b152708823d3f
Ajde <108722871+vratislavvv@users.noreply.github.com>
2023-06-18 17:44:36 +0200

    Delete letenka.py

--- Air Zochova/backend/letenka.py
+++
@@ -1,9 +0,0 @@
-class Letenka:
-
-    def __init__(self, name, surname):
-        self.name = name
-        self.surname = surname
-        print(name)
-        print(surname)
-
-y = Letenka("Vratislav", "Vojtek")
